4867279e9786f29cad236d95011f07e6ad769aa4
robpap96 <robertopapagni1996@outlook.it>
2022-11-02 17:39:30 +0100

    fatto cosnegna standard

--- js/main.js
+++ js/main.js
@@ -11,14 +11,25 @@
 "use script";
 
 const boardContainer = document.querySelector(".board");
+const playButton = document.getElementById("btn-play");
+let clicked = false;
 
-for(let i=1; i<=100; i++){
-    const boardCell = document.createElement("div");
-    boardCell.innerHTML = i;
-    boardCell.classList.add("board__number");
-    //aggiungo evento click
-    boardCell.addEventListener("", function() {
-        this.classList.add("blue__cell");
-    });
-    boardContainer.append(boardCell);
-}
+playButton.addEventListener("click", function(){
+    if(clicked == false) {
+
+        boardContainer.classList.remove("display__none");
+        for(let i=1; i<=100; i++){
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i;
+            boardCell.classList.add("board__number");
+            //aggiungo evento click
+            boardCell.addEventListener("click", function() {
+                console.log(this.innerHTML);
+                this.classList.add("blue__cell");
+            });  
+            boardContainer.append(boardCell);
+        }
+        clicked = true; 
+    }
+});
+
