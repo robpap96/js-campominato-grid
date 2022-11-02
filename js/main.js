eae07869f8937567efd16fc64e18d10b0079fa9d
robpap96 <robertopapagni1996@outlook.it>
2022-11-02 13:10:55 +0100

    aggiunto logo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 "use script";
 
 const boardContainer = document.querySelector(".board");
@@ -7,7 +17,7 @@ for(let i=1; i<=100; i++){
     boardCell.innerHTML = i;
     boardCell.classList.add("board__number");
     //aggiungo evento click
-    boardCell.addEventListener("click", function() {
+    boardCell.addEventListener("", function() {
         this.classList.add("blue__cell");
     });
     boardContainer.append(boardCell);
